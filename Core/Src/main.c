339ebfe4e62d7f24dd3b2794315bff58aea1c898
Echo-RWN <renweining070228@163.com>
2026-02-27 11:34:50 +0800

    init repo

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -133,6 +133,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
+
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+
   MX_TIM1_Init();
   MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
